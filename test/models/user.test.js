05ae0c232f2ccbbfba4b78bd8aa2d8d6bea82548
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-05 14:27:58 -0500

    Add Unit Test user

---
+++ test/models/user.test.js
@@ -0,0 +1,16 @@
+const User = require('./../../app/models/user.js')
+
+describe('Unit Test for User class', () => {
+    test('Create an user object', () =>{
+        //Instance User
+        const user = new User(1, 'LuisCasillasA', 'Luis', 'Bio', 'Date created', 'Last update')
+
+        //Validate values
+        expect(user.id).toBe(1);
+        expect(user.userName).toBe('LuisCasillasA');
+        expect(user.name).toBe('Luis');
+        expect(user.bio).toBe('Bio');
+        expect(user.dateCreated).toBe('Date created');
+        expect(user.lastUpdate).toBe('last Update');
+    })
+})
