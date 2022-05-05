c6982a8f48ebb6e96909ee7fd017e1f6807a28fc
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-05 16:52:48 -0500

    Sprint 2 test requirement 4

--- test/models/UserService.test.js
+++ test/models/UserService.test.js
@@ -27,5 +27,17 @@ describe('Unit Test for User Service class', () => {
         const user = UserService.create(1, 'LuisCasillasA', 'Luis')
         UserService.updateUsername(user, 'LuisAcevedo')
         expect(user.userName).toBe('LuisAcevedo')
+    });
+    test('4. Given a list of users give me the list of usernames', () =>{
+        //Create users
+        const user1 = UserService.create(1, 'LuisCasillasA', 'Luis')
+        const user2 = UserService.create(2, 'LuisCasillasB', 'Luis')
+        const user3 = UserService.create(3, 'LuisCasillasC', 'Luis')
+
+        //Validate values
+        const userNames = UserService.getAllUserNames([user1, user2, user3])
+        expect(userNames).toContain('LuisCasillasA')
+        expect(userNames).toContain('LuisCasillasB')
+        expect(userNames).toContain('LuisCasillasC')
     })
 })
