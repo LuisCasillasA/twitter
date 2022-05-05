204df73498076fb1b321bbd7d809a5ed7de78c0f
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-05 16:18:10 -0500

    Sprint 2 requirement 2

--- app/models/UserService.js
+++ app/models/UserService.js
@@ -4,6 +4,10 @@ class UserService{
     static create(id, userName, name){
         return new User(id, userName, name, 'Sin Bio')
     }
+    static getInfo(user){
+        this.user = [user.id, user.userName, user.name, user.bio]
+        return this.user
+    }
 }
 
 module.exports = UserService
--- test/models/UserService.test.js
+++ test/models/UserService.test.js
@@ -11,4 +11,15 @@ describe('Unit Test for User Service class', () => {
         expect(user.name).toBe('Luis');
         expect(user.bio).not.toBeUndefined();
     });
+    test('getInfo static method', () => {
+        //Create user service
+        const user = UserService.create(1, 'LuisCasillasA', 'Luis')
+
+        //Validate values
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1);
+        expect(userInfoList[1]).toBe('LuisCasillasA');
+        expect(userInfoList[2]).toBe('Luis');
+        expect(userInfoList[3]).toBe('Sin Bio');
+    })
 })
