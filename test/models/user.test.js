69fd7dc6526ef7e76689ed1d1134e0e60747fdab
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-05 14:37:35 -0500

    R2 Step 1

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,14 +3,14 @@ const User = require('./../../app/models/user.js')
 describe('Unit Test for User class', () => {
     test('Create an user object', () =>{
         //Instance User
-        const user = new User(1, 'LuisCasillasA', 'Luis', 'Bio', 'Date created', 'Last update')
+        const user = new User(1, 'LuisCasillasA', 'Luis', 'Bio')
 
         //Validate values
         expect(user.id).toBe(1);
         expect(user.userName).toBe('LuisCasillasA');
         expect(user.name).toBe('Luis');
         expect(user.bio).toBe('Bio');
-        expect(user.dateCreated).toBe('Date created');
-        expect(user.lastUpdate).toBe('Last update');
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdate).not.toBeUndefined();
     })
 })
